d6ff6ab10f8e23ab654c0044a15b6a191f2a5ebe
Triggered41 <vehdathamid@gmail.com>
2024-10-08 18:35:48 +0530

    Added buttons and logic changed so player can attack specific enemy

--- Environment/Combatants/combatant.gd
+++ Environment/Combatants/combatant.gd
@@ -6,17 +6,20 @@ class_name Combatant
 @export var initiative: int
 @export var team: int
 
-@export var has_attacked = false
 
 enum {
 	PLAYER_TEAM,
 	ENEMY_TEAM
 }
 
+static func attack(target: Combatant):
+	target.health -= randi_range(1, 10)
+
 func init() -> void:
 	health = randi_range(15, 25)
 	initiative = randi_range(4, 8)
 
-func attack(target: Combatant):
-	target.health -= randi_range(1, 10)
-	has_attacked = true
+func attack_t(target: Combatant):
+	var a = randi_range(1, 10)
+	print(name, " attacked: ", target.name, ", " ,target.health, '-> ', target.health-a)
+	target.health -= a
--- GameManger/game_manager.gd
+++ GameManger/game_manager.gd
@@ -1,53 +1,59 @@
 extends Node2D
 
 @export var pool: CombatantsPool
+@onready var control: Control = $Control
 
 var player_team : Array[Combatant]
 var enemy_team : Array[Combatant]
 
 var all_combs : Array[Combatant]
+var current_turn := 0
 
 func _ready() -> void:
-	pool.init()
+	pool = CombatantsPool.new()
+	for i in pool.POOL_SIZE:
+		var c = Combatant.new()
+		c.name = char('A'.to_ascii_buffer()[0] + i)
+		pool.pool.append(c)
 	randomize()
+	pool.init()
+	
+	var buttons = control.get_children() as Array[Button]
+	for i in buttons.size():
+		buttons[i].button_down.connect(_on_enemy_select.bind(i))
 	
 	player_team = pool.get_team(Combatant.PLAYER_TEAM)
 	enemy_team = pool.get_team(Combatant.ENEMY_TEAM)
+	
+	print("Player team: ", player_team.map(func(x): return x.name))
+	print("Enemy team: ", enemy_team.map(func(x): return x.name))
+	
 	all_combs = player_team + enemy_team
+	
 	for i in pool.TEAM_SIZE:
 		player_team[i].initiative += randi_range(-3, 3)
 		enemy_team[i].initiative += randi_range(-3, 3)
 		
 	all_combs.sort_custom(func (x, y): return x.initiative > y.initiative)
 	
+	#print("Init Before: ", all_combs.map(func(x: Combatant):return x.initiative))
+	print("\nRound Start\n")
 	start_round()
+	#print("Init Before: ", all_combs.map(func(x:Combatant):return x.initiative))
+
 
 func start_round():
-	var current_turn := 0
-	var init_count := 0
-	var current_initiative := all_combs[0].initiative
+	var is_npc_turn := true
+	while is_npc_turn:
+		is_npc_turn = start_turn()
 	
-	while current_turn < all_combs.size():
-		while current_turn+init_count < all_combs.size() and current_initiative == all_combs[current_turn+init_count].initiative:
-			init_count += 1
-		init_count -= 1
-		
-		var pick := current_turn + randi_range(0, init_count)
-		var curr := all_combs[pick]
-		if curr.team == Combatant.ENEMY_TEAM:
-			curr.attack(player_team.pick_random())
-		else:
-			pass
-		swap(current_turn, pick)
-		init_count = 0
-		current_turn += 1
-		
-		if current_turn+init_count >= all_combs.size(): return
-		current_initiative = all_combs[current_turn].initiative
 
-func start_turn(current_turn: int) -> void:
+func start_turn() -> bool:
+	if current_turn >= all_combs.size():
+		print("\nRound End!\n")
+		return false
 	var init_count := 0
-	var current_initiative := all_combs[0].initiative
+	var current_initiative := all_combs[current_turn].initiative
 	
 	while current_turn+init_count < all_combs.size() and current_initiative == all_combs[current_turn+init_count].initiative:
 		init_count += 1
@@ -56,15 +62,30 @@ func start_turn(current_turn: int) -> void:
 	var pick := current_turn + randi_range(0, init_count)
 	var curr := all_combs[pick]
 	if curr.team == Combatant.ENEMY_TEAM:
-		curr.attack(player_team.pick_random())
+		print("Enemy turn")
+		curr.attack_t(player_team.pick_random())
 		swap(current_turn, pick)
 		init_count = 0
 		current_turn += 1
 		
-		if current_turn+init_count >= all_combs.size(): return
+		if current_turn+init_count >= all_combs.size(): return false
 		current_initiative = all_combs[current_turn].initiative
+		return true
 	else:
-		pass
+		start_player_turn()
+		return false
+
+func start_player_turn():
+	pass
+	
+func _on_enemy_select(index: int) -> void:
+	if current_turn >= all_combs.size():
+		print("\nRound End!\n")
+		return
+	print("Player turn")
+	all_combs[current_turn].attack_t(enemy_team[index])
+	current_turn += 1
+	start_round()
 
 func swap(a: int, b: int) -> void:
 	if (a != b): print("Swapped")
